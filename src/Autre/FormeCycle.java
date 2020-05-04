bcf1c67f128414f11f262aaa53e8e35593563b51
Guillaume-vd <58851869+Guillaume-vd@users.noreply.github.com>
2020-05-04 08:34:37 +0200

    pour info

---
+++ src/Autre/FormeCycle.java
@@ -0,0 +1,13 @@
+package Autre;
+
+import Type.Arc;
+
+import java.util.List;
+
+public class FormeCycle {
+
+    public static boolean FC(List<Arc> ListArc, Arc ArcAjout){
+        return false;
+    }
+
+}
